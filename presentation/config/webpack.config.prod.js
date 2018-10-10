ca2b211a0c33f6249d6ae5480b481fd5305e8ce9
Nick Van Doorn <vandoorn.nick@gmail.com>
2018-10-09 23:12:15 -0700

    Load .example.html with raw-loader in prod

--- presentation/config/webpack.config.prod.js
+++ presentation/config/webpack.config.prod.js
@@ -141,7 +141,7 @@ module.exports = {
             }
           },
           {
-            test: /\.example\.js$/,
+            test: [/\.example\.js$/, /\.example\.html$/],
             loader: require.resolve('raw-loader')
           },
           // Process JS with Babel.
